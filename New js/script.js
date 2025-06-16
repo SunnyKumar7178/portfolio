ecee39f43c623a20de2b6d4681273cb947659250
Sunny kumar <ssingh6102000@gmail.com>
2025-06-16 11:14:41 +0530

    Add New JS

---
+++ New js/script.js
@@ -0,0 +1,6 @@
+    let x=10;
+    let y=20;
+    let z=x+y;
+    console.log(z);
+    //document.write("Hello Everyone!!!"); // document does not define in node js without index.html file.
+    console.log(" hello everyone!!!@");
